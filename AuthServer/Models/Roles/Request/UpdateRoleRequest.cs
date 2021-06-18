4f71de98cde2a653a103ebafb3342c76a95fbfcb
Yadab Neupane <yneupane41@outlook.com>
2021-06-18 09:29:18 +0545

    Update role for rank

--- AuthServer/Models/Roles/Request/UpdateRoleRequest.cs
+++ AuthServer/Models/Roles/Request/UpdateRoleRequest.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace AuthServer.Models.Roles.Request
+﻿namespace AuthServer.Models.Roles.Request
 {
     public class UpdateRoleRequest
     {
         public string Id { get; set; }
         public string Name { get; set; }
+        public int Rank { get; set; }
         public bool IsPublic { get; set; }
     }
 }
