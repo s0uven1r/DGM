8237646e87cf89c4c76a5bf788fc889d74ad8df0
actinic <sushil.pyakurel31@gmail.com>
2021-08-01 20:48:20 +0545

    migration on resource api and db change to sync with main branch

--- ResourceAPI/Program.cs
+++ ResourceAPI/Program.cs
@@ -1,13 +1,48 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Resource.Domain.Persistence;
+using System;
+using System.Threading.Tasks;
 
 namespace ResourceAPI
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static object Log { get; private set; }
+
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<AppDbContext>();
+
+                    if (context.Database.IsSqlServer())
+                    {
+                        context.Database.Migrate();
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+                    logger.LogError(ex, "An error occurred while migrating or seeding the database.");
+
+                    throw;
+                }
+            }
+
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
