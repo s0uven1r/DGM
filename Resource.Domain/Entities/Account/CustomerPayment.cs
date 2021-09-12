cf8cc2ae137c378c05a0ffc9baa29cdf972dab23
saugat dahal <saugatdahal20@gmail.com>
2021-09-12 08:41:43 +0545

    add course package

---
+++ Resource.Domain/Entities/Account/CustomerPayment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resource.Domain.Entities.Account
+{
+    public class CustomerPayment: BaseEntity
+    {
+        public decimal PackageAmount { get; set; }
+        public decimal PaidAmount { get; set; }
+        public bool IsDiscountAvail { get; set; }
+        public bool IsPercentDiscount { get; set; }
+        public decimal DueAmount { get; set; }
+        public decimal DiscountPercent { get; set; }
+        public decimal DiscountAmount { get; set; }
+        public decimal NetAmount { get; set; }
+       
+    }
+}
