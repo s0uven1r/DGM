38798a9f8d3f4b750d3a935cde8ecff07713c3f1
saugat dahal <saugatdahal20@gmail.com>
2021-10-05 08:11:36 +0545

    manage oidc based on environment

--- ClientApp/src/environments/environment.prod.ts
+++ ClientApp/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  apiIdentityUrl: 'https://localhost:44316/',
-  resourceUrl: 'https://localhost:44337/api/'
+  apiIdentityUrl: 'https://localhost:44316/Authorization/',
+  resourceUrl: 'https://localhost:44337/api/',
+  issuer: 'https://localhost:44316',
+  redirectUri: 'http://localhost:4200/auth-callback'
 };
