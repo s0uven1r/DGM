d2c37e67d4a8be414acc6d7afb2e138988d907fe
sumit.gautam <sumit.gautam@ekbana.info>
2021-10-17 17:37:37 +0545

    crud udpdates

---
+++ Resource.Application/Models/Account/AccountEntry/Request/AccountEntryGetSingleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resource.Application.Models.Account.AccountEntry.Request
+{
+    public class AccountEntryGetSingleViewModel
+    {
+        public string Type { get; set; }
+        public string AccountNumber { get; set; }
+        public string TransactionDateEN { get; set; }
+    }
+}
