efd71d13df5b9e2c4e0d7876517069b9a6b564c5
sumit.gautam <sumit.gautam@ekbana.info>
2021-10-17 17:35:48 +0545

    aaccount entry get single.

---
+++ Resource.Infrastructure/Persistence/Migrations/20211017113106_AccountTransaction_Columns_Updated.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Resource.Infrastructure.Persistence.Migrations
+{
+    public partial class AccountTransaction_Columns_Updated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsVehicle",
+                table: "Transactions");
+
+            migrationBuilder.RenameColumn(
+                name: "VehicleNumber",
+                table: "Transactions",
+                newName: "Type");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "TransactionDate",
+                table: "Transactions",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "TransactionDateNP",
+                table: "Transactions",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TransactionDateNP",
+                table: "TransactionDetails",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "TransactionDetails",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TransactionDate",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "TransactionDateNP",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "TransactionDateNP",
+                table: "TransactionDetails");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "TransactionDetails");
+
+            migrationBuilder.RenameColumn(
+                name: "Type",
+                table: "Transactions",
+                newName: "VehicleNumber");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsVehicle",
+                table: "Transactions",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
