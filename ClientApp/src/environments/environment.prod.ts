e35cb6ef15f24ae4e2530056ca8a7d3494797b98
sumit.gautam <sumit.gautam@ekbana.info>
2021-10-17 11:57:38 +0545

    angular production ports changed

--- ClientApp/src/environments/environment.prod.ts
+++ ClientApp/src/environments/environment.prod.ts
@@ -1,12 +1,7 @@
 export const environment = {
   production: true,
-<<<<<<< HEAD
   apiIdentityUrl: 'https://localhost:5051/Authorization/',
-  resourceUrl: 'https://localhost:6061/api/'
-=======
-  apiIdentityUrl: 'https://localhost:44316/Authorization/',
-  resourceUrl: 'https://localhost:44337/api/',
-  issuer: 'https://localhost:44316',
+  resourceUrl: 'https://localhost:6061/api/',
+  issuer: 'https://localhost:5051',
   redirectUri: 'http://localhost:4200/auth-callback'
->>>>>>> dgm
 };
