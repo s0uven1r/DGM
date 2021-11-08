8a3f18a2b5d6ba9f6953ad63135d2424d711ca0b
sumit.gautam <sumit.gautam@ekbana.info>
2021-11-08 22:50:23 +0545

    account number for user creation

--- AuthServer/Models/RegisterInputModel.cs
+++ AuthServer/Models/RegisterInputModel.cs
@@ -28,7 +28,6 @@ namespace AuthServer.Models
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
